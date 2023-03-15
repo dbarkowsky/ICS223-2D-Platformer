d929d1543e3f2f2746134c1c6278f1d2e1bb4ab9
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-15 11:50:25 -0700

    added character animation and camera following

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,13 +19,20 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded = false;
     [SerializeField] private Transform groundCheckPoint;
     [SerializeField] private LayerMask groundLayerMask;
+    private Animator anim;
     private float groundCheckRadius = 0.3f;
     private int jumpMax = 2; // 1 == single jump
     private int jumpsAvailable = 0; // remaining jumps available to player
 
+    // animation variables
+    private bool facingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        // assign animator
+        anim = gameObject.GetComponent<Animator>();
+
         // given a desired jumpHeight and jumpTime, calculate gravity (same formulas as 3D)
         float timeToApex = jumpTime / 2.0f;
         float gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
@@ -40,9 +47,18 @@ public class PlayerController : MonoBehaviour
     {
         // read (and store) horizontal input
         horizInput = Input.GetAxis("Horizontal");
+        anim.SetBool("isRunning", horizInput > 0.01 || horizInput < -0.01);
+
+        // if we're facing the opposite direction of movement, flip the player sprite
+        if ((!facingRight && horizInput > 0.01f) ||
+            (facingRight && horizInput < -0.01f))
+        {
+            Flip();
+        }
 
         // we're grounded if ground detection circle collides with ground layer and we're decending
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask) && rbody.velocity.y < 0.01;
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask) && rbody.velocity.y < 0.01; // collision and heading downwards
+        anim.SetBool("isGrounded", isGrounded);
 
         if (isGrounded)
         {
@@ -59,8 +75,6 @@ public class PlayerController : MonoBehaviour
         // We're moving via Rigidbody physics (setting velocity directly) so we need to use FixedUpdate
         float xVel = horizInput * moveSpeed * Time.deltaTime;   // determine x velocity
         rbody.velocity = new Vector2(xVel, rbody.velocity.y);   // set new x velocity, maintain existing y velocity
-
-        
     }
 
     private void Jump()
@@ -68,11 +82,19 @@ public class PlayerController : MonoBehaviour
         // maintain x velocity, change y to send player upwards
         rbody.velocity = new Vector2(rbody.velocity.x, initialJumpVelocity);
         jumpsAvailable--;
+        anim.SetTrigger("jump");
     }
 
     private void OnDrawGizmos()
     {
         // draw sphere at transform's location
         Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        // rotate 180 degrees on Y axis
+        transform.Rotate(Vector3.up * 180);
     }
 }
