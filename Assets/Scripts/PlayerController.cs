213cf98539f0d6c4bf461569afb5ba1192cfb4c2
dbarkowsky <dylanbarkowsky@gmail.com>
2023-03-15 11:16:40 -0700

    character jump and movement done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     // movement variables
-    //[SerializeField] Rigidbody2D rbody; 
-    //private float horizInput;
-    //private float moveSpeed = 450.0f;  // 4.5 * 100 since we're using Rigidbody physics to move
+    [SerializeField] Rigidbody2D rbody;
+    private float horizInput;
+    private float moveSpeed = 450.0f;  // 4.5 * 100 since we're using Rigidbody physics to move
 
     // jump variables
-    //private float jumpHeight = 3.0f;
-    //private float jumpTime = 0.75f;
-    //private float initialJumpVelocity;
+    private float jumpHeight = 3.0f;
+    private float jumpTime = 0.75f;
+    private float initialJumpVelocity;
 
+    // to keep from unlimited jumping
+    private bool isGrounded = false;
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private LayerMask groundLayerMask;
+    private float groundCheckRadius = 0.3f;
+    private int jumpMax = 2; // 1 == single jump
+    private int jumpsAvailable = 0; // remaining jumps available to player
 
     // Start is called before the first frame update
     void Start()
     {
         // given a desired jumpHeight and jumpTime, calculate gravity (same formulas as 3D)
-        //float timeToApex = jumpTime / 2.0f;
-        //float gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
+        float timeToApex = jumpTime / 2.0f;
+        float gravity = (-2 * jumpHeight) / Mathf.Pow(timeToApex, 2);
 
         // calculate jump velocity (upward motion)
-        //initialJumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);      
+        initialJumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        rbody.gravityScale = gravity / Physics2D.gravity.y;
     }
 
     // Update is called once per frame
     void Update()
     {
         // read (and store) horizontal input
-        //horizInput = Input.GetAxis("Horizontal");        
+        horizInput = Input.GetAxis("Horizontal");
+
+        // we're grounded if ground detection circle collides with ground layer and we're decending
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask) && rbody.velocity.y < 0.01;
+
+        if (isGrounded)
+        {
+            jumpsAvailable = jumpMax;
+        }
+        if (Input.GetButtonDown("Jump") && jumpsAvailable > 0)
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
         // We're moving via Rigidbody physics (setting velocity directly) so we need to use FixedUpdate
-        //float xVel = horizInput * moveSpeed * Time.deltaTime;   // determine x velocity
-        //rbody.velocity = new Vector2(xVel, rbody.velocity.y);   // set new x velocity, maintain existing y velocity
+        float xVel = horizInput * moveSpeed * Time.deltaTime;   // determine x velocity
+        rbody.velocity = new Vector2(xVel, rbody.velocity.y);   // set new x velocity, maintain existing y velocity
+
+        
+    }
+
+    private void Jump()
+    {
+        // maintain x velocity, change y to send player upwards
+        rbody.velocity = new Vector2(rbody.velocity.x, initialJumpVelocity);
+        jumpsAvailable--;
+    }
+
+    private void OnDrawGizmos()
+    {
+        // draw sphere at transform's location
+        Gizmos.DrawWireSphere(groundCheckPoint.position, groundCheckRadius);
     }
 }
